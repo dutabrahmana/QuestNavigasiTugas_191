700af66412af2379854eae08be02f3ff647a51c2
dutabrahmana <dutalinggau25@gmail.com>
2025-11-08 19:35:37 +0700

    memangail fungsi untuk menjalankan program

--- app/src/main/java/com/example/navigasiku/MainActivity.kt
+++ app/src/main/java/com/example/navigasiku/MainActivity.kt
@@ -6,28 +6,33 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.navigasiku.ui.theme.NavigasiKuTheme
 
+import com.example.navigasiku.view.Halaman1
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
         setContent {
             NavigasiKuTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    DataApp()
                 }
             }
+            }
         }
-    }
 }
 
 @Composable
@@ -40,7 +45,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun GretingPreview() {
     NavigasiKuTheme {
         Greeting("Android")
     }
