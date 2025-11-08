e7501375c1b22affb104f05d719bf8dfb70c4342
dutabrahmana <dutalinggau25@gmail.com>
2025-11-08 20:08:46 +0700

    Menambahkan fungsi ListPeserta dengan Scaffold dan Column utama. Mendefinisikan warna lokal (cardBackgroundColor dan primaryButtonColor) untuk styling.

---
+++ app/src/main/java/com/example/navigasiku/view/ListPeserta.kt
@@ -0,0 +1,57 @@
+package com.example.navigasiku.view
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.navigasiku.R
+
+@Composable
+fun ListPeserta(
+    modifier: Modifier = Modifier,
+    onBackListBtn: () -> Unit,
+    onNextListBtn: () -> Unit
+    /////
+) {
+
+    val cardBackgroundColor = Color(0xFFCDDC39) // Warna abu-abu muda di Card
+    val primaryButtonColor = Color(0xFFE10543) // Warna Biru Tua tombol
+
+    Scaffold(
+        modifier = modifier.fillMaxSize()
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+                .padding(horizontal = 24.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+         }
